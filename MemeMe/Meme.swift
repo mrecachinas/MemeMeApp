bc78f8d494001a7aa9c473a90cc520575569bbac
Mike Recachinas <mgr3yp@virginia.edu>
2015-11-06 00:28:37 -0500

    Create Meme class and add to AppDelegate

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,24 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Michael Recachinas on 7/22/15.
+//  Copyright (c) 2015 Michael Recachinas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Meme {
+    var topText: NSString!
+    var bottomText: NSString!
+    var image: UIImage!
+    var memedImage: UIImage!
+    
+    init(topText: NSString, bottomText: NSString, image: UIImage, memedImage: UIImage) {
+        self.topText = topText
+        self.bottomText = bottomText
+        self.image = image
+        self.memedImage = memedImage
+    }
+}
